5f2909ca8485a2e88c9a9ccef0595e2ec2688a0c
hstla <ree7243@gmail.com>
2022-10-08 23:02:36 +0900

    테스트코드를 JUnit에 맞게 변경

--- calculatorcli/src/test/java/org/edwith/webbe/CalculatorServiceTest.java
+++ calculatorcli/src/test/java/org/edwith/webbe/CalculatorServiceTest.java
@@ -1,49 +1,67 @@
 package org.edwith.webbe;
 
+import org.edwith.webbe.calculatorcli.ApplicationConfig;
 import org.edwith.webbe.calculatorcli.CalculatorService;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+/*RunWith == JUnit이 제공하는 어노테이션으로 JUnit은 확장기능을 가지는데,
+스프링에서는 JUnit을 확장하도록 SpringJUnit4ClassRunner.class를 제공합니다.
+해당 코드는 JUnit이 테스트 코드를 실행할 때 스프링 빈 컨테이너가 내부적으로 생성되도록 합니다.*/
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ApplicationConfig.class})
 public class CalculatorServiceTest {
+
+  /*Autowired = CalcultorServiceTest 클래스가 빈으로 관리되면서,
+   스프링 빈 컨테이너는 CalculatorService를
+  주입(Inject)할 수 있게 됩니다. 이렇게 주입된 클래스를 테스트하면 됩니다.*/
+  @Autowired
   CalculatorService calculatorService;
-  int v1 = 10;
-  int v2 = 5;
-
-  @Before
-  public void init(){
-    this.calculatorService = new CalculatorService();
-
-  }
 
   @Test
   public void plus() throws Exception {
+    int value1 =  10;
+    int value2 = 5;
 
-    int result = calculatorService.plus(v1, v2);
+    // when
+    int result = calculatorService.plus(value1, value2);
 
-    Assert.assertEquals(15, result);
-//    Assert.assertEquals(16, result);
+    // then
+    Assert.assertEquals(result, 15); // 결과와 15가 같을 경우에만 성공
   }
 
   @Test
-  public void divide() throws Exception {
+  public void divide() throws Exception{
+    // given
+    int value1 = 10;
+    int value2 = 5;
 
-    int result = calculatorService.divide(v1, v2);
+    // when
+    int result = calculatorService.divide(value1, value2);
 
-    Assert.assertEquals(2, result);
+    // then
+    Assert.assertEquals(result, 2); // 결과와 15가 같을 경우에만 성공
   }
 
   @Test
-  public void divideExcepptionTest() throws Exception {
-    v2 = 0;
+  public void divideExceptionTest() throws Exception{
+    // given
+    int value1 = 10;
+    int value2 = 0;
+
     try {
-      calculatorService.divide(v1,v2);
-    } catch (ArithmeticException ae) {
-      Assert.assertTrue(true);
-      return;
+      calculatorService.divide(value1, value2);
+    }catch (ArithmeticException ae){
+      Assert.assertTrue(true); // 이부분이 실행되었다면 성공
+      return; // 메소드를 더이상 실행하지 않는다.
     }
-//    Assert.assertFail();
+
+    Assert.assertTrue(false); // 이부분이 실행되면 무조건 실패다.
   }
-
-
 }
